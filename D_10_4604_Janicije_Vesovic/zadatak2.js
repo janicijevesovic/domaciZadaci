e427c41c7b4a5b3ebf963b079ea2d0de805166a5
janicijevesovic <janicije95@hotmail.com>
2021-05-07 21:36:03 +0200

    Dodata poruka pri deljenju sa nulom

--- D_10_4604_Janicije_Vesovic/zadatak2.js
+++ D_10_4604_Janicije_Vesovic/zadatak2.js
@@ -81,8 +81,8 @@ switch (paranBroj)
 // 4. Za uneta dva broja i karakter napraviti kalkulator koji vrši množenje ta dva broja ukoliko je uneto slovo ’m’, deljenje ukoliko je uneto slovo ’d’, sabiranje ukoliko je uneto slovo ’s’ ili oduzimanje ukoliko je uneto slovo ’o’.
 
 let prviBroj = 3;
-let drugiBroj = 10;
-let operacija = "o";
+let drugiBroj = 2;
+let operacija = "d";
 
 switch (operacija)
 {
@@ -96,8 +96,14 @@ switch (operacija)
         console.log(prviBroj * drugiBroj);
         break;
     case "d":
-        console.log(prviBroj / drugiBroj);
-        break;
+        if (drugiBroj == 0) {
+            console.log("Ne moze se deliti nulom.");
+            break;
+        }
+        else {
+            console.log(prviBroj / drugiBroj);
+            break;
+        }
     default:
         console.log("Pogresan unos");
 }
