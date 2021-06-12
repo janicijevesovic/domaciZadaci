aea2da32f64a03a71984b87ea79ff4ed787b7f99
janicijevesovic <janicije95@hotmail.com>
2021-06-12 22:30:22 +0200

    18. Domaci - Javascript

---
+++ D_18_4604_Janicije_Vesovic/script.js
@@ -0,0 +1,92 @@
+let divStanje = document.querySelector("#stanje");
+let formStanje = document.querySelector("#stanje form");
+let inputDeoNaziva = document.querySelector("#deoNaziva");
+let inputMinCena = document.querySelector("#minCena");
+let inputMaxCena = document.querySelector("#maxCena");
+let divLista = document.querySelector("#lista");
+
+
+function proveriStanjePromise(resource) {
+    return new Promise((resolve, reject) => {
+        let request = new XMLHttpRequest();
+        request.addEventListener('readystatechange', function () {
+            if (this.readyState == 4 && this.status == 200) {
+                let data = JSON.parse(this.responseText);
+                resolve(data);
+            }
+            else if (this.readyState == 4) {
+                reject("Could not fetch data!");
+            }
+        });
+        request.open('GET', resource);
+        request.send();
+    });
+}
+
+let proveriStanje = e => {
+    e.preventDefault();
+    let deoNaziva = inputDeoNaziva.value.toLowerCase();
+    let minCena = inputMinCena.value;
+    let maxCena = inputMaxCena.value;
+    let itemsInStock = [];
+    let itemsStock = [];
+    // let listaArtikala = document.createElement('ul');
+    let tabelaArtikala = document.createElement('table');
+    let prviRed = document.createElement('tr');
+
+    let prvaKolona = document.createElement('th');
+    prvaKolona.textContent = "Naziv artikla:";
+    prviRed.appendChild(prvaKolona);
+
+    let drugaKolona = document.createElement('th');
+    drugaKolona.textContent = "Stanje u magacinu:";
+    prviRed.appendChild(drugaKolona);
+
+    let trecaKolona = document.createElement('th');
+    trecaKolona.textContent = "Cena:";
+    prviRed.appendChild(trecaKolona);
+
+    tabelaArtikala.appendChild(prviRed);
+
+    proveriStanjePromise("./JSON/stock.json").then(data => {
+        data.forEach(item => {
+            if (item.stock != 0) {
+                itemsInStock.push(item.id);
+                itemsStock.push(item.stock);
+            }
+        });
+        console.log(itemsInStock);
+        console.log(itemsStock);
+        return proveriStanjePromise("./JSON/prices.json");
+        }).then(data => {
+            data.forEach(item => {
+                if (itemsInStock.includes(item.id) && item.item.includes(deoNaziva) && minCena < item.price && maxCena > item.price) {
+                    // let noviArtikal = document.createElement('li');
+                    // noviArtikal.textContent = item.item;
+                    // listaArtikala.appendChild(noviArtikal);
+                    let noviRed = document.createElement('tr');
+
+                    let prvaKolona = document.createElement('td');
+                    prvaKolona.textContent = item.item;
+                    noviRed.appendChild(prvaKolona);
+
+                    let drugaKolona = document.createElement('td');
+                    let indeks = itemsInStock.indexOf(item.id);
+                    drugaKolona.textContent = itemsStock[indeks];
+                    noviRed.appendChild(drugaKolona);
+
+                    let trecaKolona = document.createElement('td');
+                    trecaKolona.textContent = item.price;
+                    noviRed.appendChild(trecaKolona);
+
+                    tabelaArtikala.appendChild(noviRed);
+                }
+            });
+            // divLista.appendChild(listaArtikala);
+            divLista.appendChild(tabelaArtikala);
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+formStanje.addEventListener('submit', proveriStanje);
