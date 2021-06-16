e5c85e5cdf3d7ce1af91c3c7961c5318285fa2a1
janicijevesovic <janicije95@hotmail.com>
2021-06-16 17:14:11 +0200

    Dodat 19. domaci

--- D_19_4604_Janicije_Vesovic/main.js
+++ D_19_4604_Janicije_Vesovic/main.js
@@ -21,26 +21,22 @@ let addMovie = (movieID, movie) => {
         console.log("Error adding movie:", error);
     });
 }
-
 addMovie("movie-02", newMovie);
 
 newMovie = new Movie("Goodfellas", {name: "Martin", surname: "Scorsese"}, 1990, ["Biography", "Crime", "Drama"], 8.7);
 addMovie("movie-03", newMovie);
 
-// Menjanje podataka o nekim filmovima u bazi
-
-// let movie = database.doc('movies/movie-02');
-// movie.update({
-//     "director.name": "Robert"
-// });
 
 let updateMovie = (movieID, property, newValue) => {
     let movie = database.doc(`movies/${movieID}`);
-    movie.update({
-        property: newValue
-    });
+    let obj = {};
+    obj[`${property}`] = newValue;
+    movie.update(obj)
+    .then(() => {console.log("Successfully")})
+    .catch(err => {console.log("Error:", err)});
 }
-
+// updateMovie("movie-03", "rating", 6.6);
+// updateMovie("movie-02", "release_year", 2001);
 
 // Dodavanje zanra nekom filmu
 
@@ -50,9 +46,7 @@ let addGenre = (movieID, newValue) => {
         genres: firebase.firestore.FieldValue.arrayUnion(newValue)
     });
 }
-
 // addGenre("movie-02", "Drama");
-
 
 // Brisanje zanra nekom filmu
 
@@ -62,6 +56,11 @@ let deleteGenre = (movieID, value) => {
         genres: firebase.firestore.FieldValue.arrayRemove(value)
     })
 }
-
 // deleteGenre("movie-02", "Drama");
+
+// Menja ime ili prezime nekom reziseru
+
+// updateMovie("movie-02", "director.name", "Novo ime");
+// updateMovie("movie-02", "director.surname", "Novo prezime");
+
 
